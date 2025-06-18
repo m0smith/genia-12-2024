929e2333c5e041416a1965a1d2e350baff935315
Matthew O. Smith <m0smith@yahoo.com>
2025-06-18 08:38:57 -0600

    Add tic-tac-toe game example

---
+++ genia/hosted/tictactoe.py
@@ -0,0 +1,30 @@
+def create_board():
+    return [' '] * 9
+
+def print_board(board):
+    for i in range(0, 9, 3):
+        row = '|'.join(board[i:i+3])
+        print(row)
+        if i < 6:
+            print('-+-+-')
+
+def set_board(board, index, value):
+    index = int(index)
+    if 0 <= index < len(board) and board[index] == ' ':
+        board[index] = value
+        return True
+    return False
+
+def check_win(board, player):
+    combos = [
+        (0,1,2), (3,4,5), (6,7,8),
+        (0,3,6), (1,4,7), (2,5,8),
+        (0,4,8), (2,4,6)
+    ]
+    for a,b,c in combos:
+        if board[a] == board[b] == board[c] == player:
+            return True
+    return False
+
+def board_full(board):
+    return all(cell != ' ' for cell in board)
